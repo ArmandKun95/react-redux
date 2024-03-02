e4d31e9f7b890c44598998332b7e3a880446a354
Armando Acosta <armajosira@gmail.com>
2024-03-02 17:01:49 -0400

    firts commit

---
+++ src/store/users/slice.ts
@@ -0,0 +1,49 @@
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
+
+export type UserId = string
+
+export interface User {
+    name: string;
+    email: string;
+    github: string;
+}
+
+export interface UserWithId extends User {
+    id: UserId;
+}
+
+const initialState: UserWithId[] = [
+	{
+		id: "1",
+		name: "Yazman Rodriguez",
+		email: "yazmanito@gmail.com",
+		github: "yazmanito",
+	},
+	{
+		id: "2",
+		name: "John Doe",
+		email: "leo@gmail.com",
+		github: "leo",
+	},
+	{
+		id: "3",
+		name: "Haakon Dahlberg",
+		email: "haakon@gmail.com",
+		github: "haakon",
+	},
+];
+
+export const usersSlice = createSlice({
+    name: "users",
+    initialState,
+    reducers: {
+        deleteUserById: (state, action: PayloadAction<UserId>) => {
+            const id = action.payload
+            return state.filter((user) => user.id !== id)
+        }
+    }
+})
+
+export default usersSlice.reducer
+
+export const {deleteUserById} = usersSlice.actions
